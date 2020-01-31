ef72b9175053742a239fd72cbafed995b1903447
PaulWattellierVitis <paul.wattellier@videofutur.com>
2020-01-31 17:30:25 +0100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "app-ideaistry-prod-serverlessdeploymentbucket-tlwktemql561"
+  },
+  apiGateway: {
+    REGION: "us-east-1 ",
+    URL: "https://sq1xzavsac.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1 ",
+    USER_POOL_ID: "us-east-1_wGgcq2pBM",
+    APP_CLIENT_ID: "7vliva5eo2qv4u4eukf7j2r5i8 ",
+    IDENTITY_POOL_ID: "us-east-1:5906fac5-1d21-45e3-80c9-36cc4cfc75c4"
+  }
+};
